4070e8d98c70fb543e190e3105647ab19a5563f6
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-10 06:47:23 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(josh)
+  puts "Hi, my name is #{josh}."
+end
+
+def introduction_with_language(josh, ruby)
+  puts "Hi, my name is #{josh} and I am learning to program in #{ruby}."
+end
